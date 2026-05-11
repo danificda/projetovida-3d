815f641ba906fe6542956c3dba9eb4da4f3d1f62
danificda <danieli.vieira.anderson@escola.pr.gov.br>
2026-05-11 15:22:45 +0000

    11/05

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const botoens = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
 for (let i =0; i < botoes.length; i++){
-    botoes[i].onclick = function (){
-        botoes[i].classlist.add("ativo")
+    botoes[i].onclick = function(){
+        for(let j=0; j<botoes.length; j++){ 
+           botoes
+        
+        }
+            botoes[i].classlist.add("ativo")
+        textos[i].classlist.add("ativo")
     }
 console.log(botoes);
 }
